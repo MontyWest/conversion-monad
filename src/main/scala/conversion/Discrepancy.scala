b376ec0f820092f0a7a4a15035a898c2da38f9a6
montywest <monty.p.west@gmail.com>
2021-07-05 20:45:27 +0100

    add discrepancies

--- src/main/scala/conversion/Discrepancy.scala
+++ src/main/scala/conversion/Discrepancy.scala
@@ -1,7 +1,17 @@
 package conversion
 
+import java.time.Instant
+import model._
+
 sealed trait Discrepancy
 
 object Discrepancy {
-	
+	case class OwnershipRemovedMissingCustomerId(removedFrom: Instant) 
+		extends Discrepancy
+
+  case class OwnershipsRemovedDuplicatedFrom(removed: List[MeterOwnerModel]) 
+  	extends Discrepancy
+
+  case object MissingOwnershipHistory
+  	extends Discrepancy
 }
