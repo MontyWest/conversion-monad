f44d5bdde8e763ee60c38e07cf69388c3c79bb4b
montywest <monty.p.west@gmail.com>
2021-07-05 16:48:20 +0100

    add creation functions for Conversion

--- src/main/scala/conversion/Conversion.scala
+++ src/main/scala/conversion/Conversion.scala
@@ -13,3 +13,13 @@ final case class Conversion[D, A](
 	discrepancies: List[D],
 	converted: A
 )
+
+object Conversion {
+
+	def tell[D](discrepancies: List[D]): Conversion[D, Unit] =
+    Conversion(discrepancies, ())
+
+  def value[D, A](x: A): Conversion[D, A] = 
+  	Conversion(Nil, x)
+
+}
