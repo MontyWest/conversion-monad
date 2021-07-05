d4b91517ffae359a54f1773d63c34ff4980bccda
montywest <monty.p.west@gmail.com>
2021-07-05 20:44:34 +0100

    utilise Conversion monad in conversion function, basic structure

--- src/main/scala/conversion/package.scala
+++ src/main/scala/conversion/package.scala
@@ -1,5 +1,7 @@
 import model._
 import kafka._
+import cats.implicits._
+
 
 package object conversion {
 
@@ -9,17 +11,24 @@ package object conversion {
 
 		def filterMissingCustomerId(
 			ownership: List[MeterOwner]
-		): Conversion[Discrepancy, List[MeterOwnerModel]]
+		): Conversion[Discrepancy, List[MeterOwnerModel]] = ???
 
 		def removeDuplicateFroms(
 			ownership: List[MeterOwnerModel]
-		): Conversion[Discrepancy, List[MeterOwnerModel]]
+		): Conversion[Discrepancy, List[MeterOwnerModel]] = ???
 
 		def validateNonEmpty(
 			ownership: List[MeterOwnerModel]
-		): Conversion[Discrepancy, Unit]
+		): Conversion[Discrepancy, Unit] = ???
 
-		???
+		for {
+			converted <- filterMissingCustomerId(event.ownership)
+			filtered  <- removeDuplicateFroms(converted)
+			_         <- validateNonEmpty(filtered)
+		} yield Model(
+			event.meterId,
+			filtered
+		)
   }
 
 }
